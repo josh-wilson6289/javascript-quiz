9ed6a00ce416d048e2bb63d5255964c533c56c46
Josh Wilson <joshua.wilson6289@gmail.com>
2020-08-11 15:22:26 -0500

    created elements for first question

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 // create needed HTML elements to variables
-
 var highScores = document.getElementById("high-scores");
 var timeLeft = document.getElementById("time-left");
 var startGame = document.getElementById("start-game");
 var startBtn = document.getElementById("start-btn");
+
+var body = document.body
 
 var secondsLeft = 60;
 
@@ -14,33 +15,70 @@ function start() {
   askFirstQuestion();
 }
 
+// displays timer on screen.  will add more if statements to decrement/clear depending on answers
 function startTimer() {
   var timerInterval = setInterval(function() {
     secondsLeft--;
     timeLeft.textContent = "Time left: " + secondsLeft;
   
   if(secondsLeft === 0) {
-    clearInterval(timerInterval);
-    alert("Time's up!");
+    stopTimer();
   }
   }, 1000);
 }
 
+function stopTimer() {
+  alert("Time's up!");
+  clearInterval(timerInterval);
+}
 
+// create html elements for first question
+function askFirstQuestion() {
+  var container = document.createElement("div");
+  var row = document.createElement("div");
+  var questionNumber = document.createElement("h1");
+  var question = document.createElement("p");
+  var choiceDiv = document.createElement("div");
+  var choice1 = document.createElement("button");
+  var choice2 = document.createElement("button");
+  var choice3 = document.createElement("button");
+  var choice4 = document.createElement("button");
 
+  // set text for elements
+  questionNumber.textContent = "Question 1";
+  question.textContent = "Commonly used data types DO NOT include ";
+  choice1.textContent = "Strings";
+  choice2.textContent = "Booleans";
+  choice3.textContent = "Alerts";
+  choice4.textContent = "Numbers";
 
+  // Append elements
+  body.appendChild(container);
+  container.appendChild(row);
+  container.appendChild(questionNumber);
+  container.appendChild(question);
+  container.appendChild(choiceDiv);
+  choiceDiv.appendChild(choice1);
+  choiceDiv.appendChild(choice2);
+  choiceDiv.appendChild(choice3);
+  choiceDiv.appendChild(choice4);
 
-
-
-
-
-
-
-
-
+  // make buttons btn-primary bootstrap buttons
+  container.classList.add("container-fluid");
+  row.classList.add("row");
+  row.classList.add("justify-content-center");
+  choiceDiv.classList.add("choice-btns");
+  choice1.classList.add("btn");
+  choice1.classList.add("btn-primary");
+  choice2.classList.add("btn");
+  choice2.classList.add("btn-primary");
+  choice3.classList.add("btn");
+  choice3.classList.add("btn-primary");
+  choice4.classList.add("btn");
+  choice4.classList.add("btn-primary");
+}
 
 
 
 // click to start game
-
 startBtn.addEventListener("click", start);
