08cbd3442388982ef72dc00bad6204dd5d8e13f2
Josh Wilson <joshua.wilson6289@gmail.com>
2020-08-12 07:39:56 -0500

    added click events for created html answer buttons.  verify answer

--- script.js
+++ script.js
@@ -4,25 +4,35 @@ var timeLeft = document.getElementById("time-left");
 var startGame = document.getElementById("start-game");
 var startBtn = document.getElementById("start-btn");
 
-var body = document.body
+var body = document.body;  
 
 var secondsLeft = 30;
 
 // creates an object for all questions
-var listOfquestions = [{
-  question: "Question 1", 
+var listOfQuestions = [{
+  header: "Question 1",
+  question: "Here is question 1", 
   choice1: "Choice 1A",                   
   choice2: "Choice 2A",                 
   choice3: "Choice 3A",                  
   choice4: "Choice 4A",
   correctAnswer: "Choice 1A"
 },
-  {question: "Question 2",
+  {header: "Question 2",
+  question: "Here is question 2",
   choice1: "Choice 2A",
   choice2: "Choice 2B",
   choice3: "Choice 2C",
   choice4: "Choice 2D",
   correctAnswer: "Choice 2B"
+},
+  {header: "Question 3",
+  question: "Here is question 3",
+  choice1: "Choice 3A",
+  choice2: "Choice 3B",
+  choice3: "Choice 3C",
+  choice4: "Choice 3D",
+  correctAnswer: "Choice 3C"
 }];
 
 questionNumber = 0;
@@ -58,20 +68,30 @@ function stopTimer() {
   renderTime();
 }
 
-// create html elements for first question
+// create html elements for each question
 function nextQuestion() {
+  if (questionNumber > 2) {
+    console.log("quiz over");
+  }
+  else {
+
   var container = document.createElement("div");
   var row = document.createElement("div");
-  var questionP = document.createElement("p");
+  var header = document.createElement("h1")
+  var question = document.createElement("p");
   var choiceDiv = document.createElement("div");
   var choice1 = document.createElement("button");
   var choice2 = document.createElement("button");
   var choice3 = document.createElement("button");
   var choice4 = document.createElement("button");
-  var currentQuestion = listOfquestions[questionNumber];
+
+  // takes the current question object
+  var currentQuestion = listOfQuestions[questionNumber];
+  var currentAnswer = currentQuestion.correctAnswer;
 
   // set text for elements
-  questionP.textContent = currentQuestion.question;
+  header.textContent = currentQuestion.header;
+  question.textContent = currentQuestion.question;
   choice1.textContent = currentQuestion.choice1;
   choice2.textContent = currentQuestion.choice2;
   choice3.textContent = currentQuestion.choice3;
@@ -79,8 +99,9 @@ function nextQuestion() {
 
   // Append elements
   body.appendChild(container);
+  container.appendChild(header);
   container.appendChild(row);
-  container.appendChild(questionP);
+  container.appendChild(question);
   container.appendChild(choiceDiv);
   choiceDiv.appendChild(choice1);
   choiceDiv.appendChild(choice2);
@@ -95,7 +116,29 @@ function nextQuestion() {
   choice1.className = "btn btn-primary";
   choice2.className = "btn btn-primary";
   choice3.className = "btn btn-primary";
-  choice4.className = " btn btn-primary";
+  choice4.className = "btn btn-primary";
+  
+  choice1.addEventListener("click", checkAnswer);
+  choice2.addEventListener("click", checkAnswer);
+  choice3.addEventListener("click", checkAnswer);
+  choice4.addEventListener("click", checkAnswer);
+
+  }
+    //checks if the button clicked is the correct answer
+    function checkAnswer() {
+      console.log(this.textContent);
+      console.log(currentAnswer);
+      if (this.textContent === currentAnswer) {
+        questionNumber++;
+        container.remove();
+        nextQuestion();
+      }
+      else {
+        console.log("incorrect answer")
+      }
+
+      
+    }
 }
 
 
